ad5c32db6aba1119e7ba872374bc4bc88f121fa5
yilungao <yilungao@gmail.com>
2018-04-08 06:40:25 -0700

    Color design

--- app/src/main/java/yilungao/gmail/com/eganwarmingcenter/mValueDependentColor.java
+++ app/src/main/java/yilungao/gmail/com/eganwarmingcenter/mValueDependentColor.java
@@ -15,10 +15,10 @@ public class mValueDependentColor implements ValueDependentColor<DataPoint> {
     }
 
     public int get(DataPoint data) {
-        if (data.getY()/max <= 0.333333333333) {
-            return Color.rgb((int) (255 * 3 * data.getY() / max), 255, 0);
-        } else {
-            return Color.rgb(255, (int) ((255 * 3 * ((((max - data.getY()) / max)))) / 2), 0);
-        }
+        if (data.getY()/max <= 0.5) {
+            return Color.rgb((int) (50 + 205 * 2 * data.getY() / max), 255, 50);
+        } else  if (data.getY() <= max){
+            return Color.rgb(255, (int) (50 + 205 * 2 * ((((max - data.getY()) / max)))), 50);
+        } else return Color.rgb(0, 0, 0);
     }
 }
